028510923408086f5128253a6418c9227a585e4a
Avic-online <avicdon1@gmail.com>
2023-11-29 12:13:06 +0100

    correction to task 3
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -8,7 +8,7 @@ lastDigit = int(last_string_digit)
 if lastDigit > 5 :
     print('last digit of', str(number), 'is', str(lastDigit), 'and is greater than 5')
 elif lastDigit == 0 :
-    print('last digit of', str(number), 'is', str(lastDigit), 'and is zero')
+    print('last digit of', str(number), 'is', str(lastDigit), 'and is 0')
 elif lastDigit < 6 and lastDigit != 0 :
     print('last digit of', str(number), 'is', str(lastDigit), 'and is less than 6 and not 0')
 else:
