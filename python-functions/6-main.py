472ad1add4dd65d5904aa200d07994ae7eff50be
Avic-online <avicdon1@gmail.com>
2023-11-30 23:44:02 +0100

    okay
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- python-functions/6-main.py
+++ python-functions/6-main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 validate_password = __import__('6-password').validate_password
+
+
 print(validate_password("Password123"))
 print(validate_password("abc123"))
 print(validate_password("Password 123"))
--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,2 +1,15 @@
 def validate_password(password):
+    if len(password) < 8:
+        return False
+    # elif password.lower():
+    elif password.islower():
+        return False
+    elif password.isupper():
+        return False
+    elif password.isdigit():
+        return False
+    elif " " in password:
+        return False
+    else:
+        return True
     
