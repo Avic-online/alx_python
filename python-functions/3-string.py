663216031a335d7e6d13ee4fe49ca178e5407eef
Avic-online <avicdon1@gmail.com>
2023-11-30 16:33:22 +0100

    poopp
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -1,1 +1,10 @@
-# here is the fuction
+# here is the function
+def reverse_string(string):
+    # string1 = "Hello"
+    # string2 = " "
+    # string3 = "madam"
+    # string4 = "Hello, World!"
+    string = reversed(string)
+    return string
+
+print(reverse_string("Hello"))
