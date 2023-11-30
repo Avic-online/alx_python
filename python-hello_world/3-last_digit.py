7159f8bdd38844afd9665d4e949a637c8a0fc152
Avic-online <avicdon1@gmail.com>
2023-11-30 13:31:45 +0100

    okayyyy
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -6,10 +6,10 @@ number_string = repr(number)
 last_string_digit = number_string[-1]
 lastDigit = int(last_string_digit)
 if lastDigit > 5 :
-    print('Last digit of', str(number), 'is', str(lastDigit), 'and is greater than 5')
+    print('Last digit of', number, 'is', lastDigit, 'and is greater than 5')
 elif lastDigit == 0 :
-    print('Last digit of', str(number), 'is', str(lastDigit), 'and is 0')
-elif lastDigit < 6 and lastDigit != 0 :
-    print('Last digit of', str(number), 'is', str(lastDigit), 'and is less than 6 and not 0')
+    print('Last digit of', number, 'is', lastDigit, 'and is 0')
+elif lastDigit < 6 and lastDigit != 0 and number<0:
+    print('Last digit of', number, 'is', -lastDigit, 'and is less than 6 and not 0')
 else:
     pass
