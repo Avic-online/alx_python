82718ff7b5bd170c9f7570dd7e32f7060c1952ba
Avic-online <avicdon1@gmail.com>
2023-11-30 13:39:50 +0100

    okay poppp
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -9,7 +9,7 @@ if lastDigit > 5 :
     print('Last digit of', number, 'is', lastDigit, 'and is greater than 5')
 elif lastDigit == 0 :
     print('Last digit of', number, 'is', lastDigit, 'and is 0')
-elif lastDigit < 6 and lastDigit != 0 and number<0:
-    print('Last digit of', number, 'is', -lastDigit, 'and is less than 6 and not 0')
+elif lastDigit < 6 and lastDigit != 0 :
+    print('Last digit of', number, 'is', lastDigit, 'and is less than 6 and not 0')
 else:
     pass
