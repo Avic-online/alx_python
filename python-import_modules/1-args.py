b9feca1946a059765673ea3e956dabc64190bf82
Avic-online <avicdon1@gmail.com>
2023-12-07 12:14:21 +0100

    ttttt
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,21 +1,21 @@
 # argument_printer.py
 
-if __name__ == "__main__":
-    import sys
+# if __name__ == "__main__":
+#     import sys
 
-    # Get the number of arguments
-    num_arguments = len(sys.argv) - 1
+#     # Get the number of arguments
+#     num_arguments = len(sys.argv) - 1
 
-    # Print the number of arguments and their list
-    print(f"Number of argument(s): {num_arguments}", end="")
+#     # Print the number of arguments and their list
+#     print(f"Number of argument(s): {num_arguments}", end="")
     
-    if num_arguments == 1:
-        print(f", followed by:\n1: {sys.argv[1]}")
-    elif num_arguments > 1:
-        print(f", followed by:")
+#     if num_arguments == 1:
+#         print(f", followed by:\n1: {sys.argv[1]}")
+#     elif num_arguments > 1:
+#         print(f", followed by:")
 
-        # Print each argument with its position
-        for i in range(1, num_arguments + 1):
-            print(f"{i}: {sys.argv[i]}")
-    else:
-        print(".")
+#         # Print each argument with its position
+#         for i in range(1, num_arguments + 1):
+#             print(f"{i}: {sys.argv[i]}")
+#     else:
+#         print(".")
---
+++ python-import_modules/2-variable_load.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    # from variable_load_2 import a
+    import variable_load_2
+    a=98
+    print("{}".format(a)) 
