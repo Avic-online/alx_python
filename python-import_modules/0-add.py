b78c851dc343538ecfa461faa691dff93fd6e219
Avic-online <avicdon1@gmail.com>
2023-12-07 07:53:41 +0100

    okay
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,7 @@
-from add_0 import add
-a = 1
-b = 2
-sum = a + b
-print("{} + {} = {}".format(a,b,sum))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    sum = a + b
+    print("{} + {} = {}".format(a,b,sum))
+# end of program
