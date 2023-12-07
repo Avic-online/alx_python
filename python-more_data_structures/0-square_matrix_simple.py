14934e5c7ad0b9e6693c4a644e879316f6852882
Avic-online <avicdon1@gmail.com>
2023-12-07 22:50:04 +0100

    efr3g3
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,5 @@
 def square_matrix_simple(matrix=[]):
-        # Create a new matrix with the same size as the input matrix
     result_matrix = [list(row) for row in matrix]
-
-    # Iterate through each element of the input matrix and compute the square
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             result_matrix[i][j] = matrix[i][j] ** 2
