68afad44409ced46379da7bcaf7c652aeea14ce6
Avic-online <avicdon1@gmail.com>
2023-12-07 23:24:00 +0100

    okay
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,16 +1,19 @@
 if __name__ == "__main__":
     import sys
 
-    argv = ["Hello", "Holberton", "School", 98, "Battery", "street"]
-
+    # Get the number of arguments
     num_arguments = len(sys.argv) - 1
-    print("Number of argument(s): {}".format(num_arguments), end="")
+
+    # Print the number of arguments and their list
+    print(f"Number of argument(s): {num_arguments}", end="")
     
     if num_arguments == 1:
         print(f", followed by:\n1: {sys.argv[1]}")
     elif num_arguments > 1:
         print(f", followed by:")
-        for i in range(argv[6]):
+
+        # Print each argument with its position
+        for i in range(1, num_arguments + 1):
             print(f"{i}: {sys.argv[i]}")
     else:
         print(".")
