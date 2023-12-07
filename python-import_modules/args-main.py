193c6f84cdb7273319986a4c4bad0800250d49a7
Avic-online <avicdon1@gmail.com>
2023-12-07 15:13:01 +0100

    muuuu
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- python-import_modules/args-main.py
+++ python-import_modules/args-main.py
@@ -1,7 +1,13 @@
-def list_arg (*argv):
-    argv = ["Hello", "Holberton", "School", 98, "Battery", "street"]
-    start = 0
-    for start in argv:
-        print("this is it {}".format(argv), end="")
-        start = start + 1
-    return argv
+# def list_arg (*argv):
+#     argv = ["Hello", "Holberton", "School", 98, "Battery", "street"]
+#     start = 0
+#     for start in argv:
+#         print("this is it {}".format(argv), end="")
+#         start = start + 1
+#     return argv
+
+
+a = [1, 2, 3, 4]
+b = a
+a[2] = 10
+b
