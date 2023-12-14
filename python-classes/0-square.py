7f93078d87184f711db587f8aace65208e01ec1a
Avic-online <avicdon1@gmail.com>
2023-12-14 12:43:39 +0100

    okay
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,10 @@
 class Square:
+
+    """
+    Private instance attribute: size
+    Instantiation with size (no type/value verification)
+    You are not allowed to import any module
+    """
+
     def __init__(self, size):
         self.__size = size
