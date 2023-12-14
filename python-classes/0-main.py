6b822238b068adb61a454881d9a40704334d5b45
Avic-online <avicdon1@gmail.com>
2023-12-14 12:49:44 +0100

    uuu
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- python-classes/0-main.py
+++ python-classes/0-main.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 Square = __import__('0-square').Square
-
+# the square is now imported from the 0-square folder
 my_square = Square(3)
 print(type(my_square))
 print(my_square.__dict__)
+
+# above is where the printing is done
+
 
 try:
     print(my_square.size)
