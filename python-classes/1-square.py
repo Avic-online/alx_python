15b1b05a686a4bfd69a1f5cfe168f92d3d6d1947
Avic-online <avicdon1@gmail.com>
2023-12-14 13:13:57 +0100

    okay
    Signed-off-by: Avic-online <avicdon1@gmail.com>

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+class Square:
+    def __init__(self, size):
+        self.__size = size
+        self.__validate_size()
+    def __validate_size(self):
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
