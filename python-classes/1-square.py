d41de605ba538b8151e58b7b9538a9688abc1b6f
Avic-online <avicdon1@gmail.com>
2023-12-14 14:21:44 +0100

    okay
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,13 +1,20 @@
 class Square:
-    """ python3 -c 'print(__import__("my_module").MyClass.__doc__)' """
-    def __init__(self, size):
-        """ python3 -c 'print(__import__("my_module").my_function.__doc__)'
-        and python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'. """
+    """
+    python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+    """
+    def __init__(self, size=0):
+        """
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        and python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'.
+        """
         self.__size = size
         self.__validate_size()
+
     def __validate_size(self):
-        """ python3 -c 'print(__import__("my_module").my_function.__doc__)'
-        and python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)' """
+        """
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        and python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'.
+        """
         if not isinstance(self.__size, int):
             raise TypeError("size must be an integer")
         elif self.__size < 0:
