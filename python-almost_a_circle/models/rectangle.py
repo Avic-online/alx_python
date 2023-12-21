6ad677f323d762fed75db7fcd2b3d18c740ea83f
Avic-online <avicdon1@gmail.com>
2023-12-21 23:11:00 +0100

    okaayyy
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -135,6 +135,7 @@ class Rectangle(Base):
         this is to set height of with a self arg and a value
         argument declared on it
         """
+        self.__height = value
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value <= 0:
