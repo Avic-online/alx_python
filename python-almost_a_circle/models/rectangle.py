5309ab57a5726138b336c77397e6f9ca60382d80
Avic-online <avicdon1@gmail.com>
2023-12-21 04:18:14 -0600

    tast one

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,68 @@
+"""
+this is a documentation of the module
+here we will import the Base class from our python base file
+and do some inheritance
+"""
+from base import Base
+
+class Rectangle(Base):
+    """
+    This is a class Rectangle that inherits from another class,
+    now its super class, Base
+    """
+
+    @property
+    def width(self):
+        return self.__width
+        """
+        this is to get the variable width
+        """
+    
+    @width.setter
+    def width(self, value):
+        self.__width = value
+        """
+        this is to set the variable width
+        """
+    @property
+    def height(self):
+        return self.__height
+        """
+        this is to get height
+         """
+    
+    @height.setter
+    def height(self, value):
+        self.__height = value
+        #this is to set height
+    @property
+    def x(self):
+        return self.__x
+        # this is to get x
+    
+    @x.setter
+    def x(self, value):
+        self.__x = value
+        # this is to set x
+
+    @property
+    def y(self):
+        return self.__y
+        #this is to get y
+    
+    @y.setter
+    def y(self, value):
+        self.__y = value
+        #this is to set y
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Class constructor for Rectangle.
+        this function initializes itself with arguments
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
