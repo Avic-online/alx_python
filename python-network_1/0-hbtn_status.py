3865fc7be64c4abe4554a2edd47d3fc16c40a566
Avic-online <avicdon1@gmail.com>
2023-12-21 06:38:59 -0600

    amendddd

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -7,8 +7,14 @@ import requests
 
 url = https://alu-intranet.hbtn.io/status
 
+# url that we are to link to
+
 response = requests.get(url)
 
+# this is the method, get, used
+
 print(response.status_code)
+
+# our print statement
 
 print(response.text)
