bd580204a82cf0da7d5533835255d3cdd3c5f777
Avic-online <avicdon1@gmail.com>
2024-01-25 11:03:02 +0100

    readme

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,5 +7,5 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELEXT * FROM 'states'")
+    cursor.execute("SELECT * FROM 'states'")
     [print(state) for state in cursor.fetchall()]
