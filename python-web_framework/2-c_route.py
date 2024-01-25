a6e05b83ac171281ac90eaffd7fe7ba4d13717bd
Avic-online <avicdon1@gmail.com>
2024-01-25 16:53:04 +0100

    done

--- python-web_framework/2-c_route.py
+++ python-web_framework/2-c_route.py
@@ -18,8 +18,9 @@ def hbnb():
 
 @app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
+    formatted_text = text.replace('_', ' ')
     print(text)
-    return render_template("copy_route.html", text=text)
+    return render_template("copy_route.html", formatted_text=formatted_text)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port="5000")
