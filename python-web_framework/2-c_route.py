b5dd3cd9a3eaab7868a2bb84782c4fd8830eb4b7
Avic-online <avicdon1@gmail.com>
2024-01-25 16:27:42 +0100

    copy

---
+++ python-web_framework/2-c_route.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python3
+"""
+    basic flask web server on avic's
+    virtual environment
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def home():
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    print(text)
+    return render_template("copy_route.html", text=text)
+
+if __name__ == "__main__":
+    app.run(debug=True)
