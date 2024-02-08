d5d402dc3cd00da03c73f4e09f1156682da25c47
Avic-online <avicdon1@gmail.com>
2024-02-09 00:22:23 +0100

    o status
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,20 +1,27 @@
-"""
-This program will import a request
-it is using a specific uniform resource locator and header
-"""
+#!/usr/bin/python3
+# This program will import a request
+# it is using a specific uniform resource locator and header
 
-import requests
+import urllib.request
 
-url = https://alu-intranet.hbtn.io/status
+# url = https://alu-intranet.hbtn.io/status
 
-# url that we are to link to
+# # url that we are to link to
 
-response = requests.get(url)
+# response = requests.get(url)
 
-# this is the method, get, used
+# # this is the method, get, used
 
-print(response.status_code)
+# print(response.status_code)
 
-# our print statement
+# # our print statement
 
-print(response.text)
+# print(response.text)
+if __name__ == "__main__":
+    request = urllib.request.Request('https://alu-intranet.hbtn.io/status')
+    with urllib.request.urlopen(request) as response:
+        body = response.res()
+        print('Body response:')
+        print('\t- type: {}'.format(type(body)))
+        print('\t- content: {}'.format(body))
+        print('\t- utf8 content: {}'.format(body.decode('utf-8')))
