d77bcf8602e384c738984fbd13390c109c64784c
Avic-online <avicdon1@gmail.com>
2024-02-09 00:27:45 +0100

    alright
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,24 +3,14 @@
 # it is using a specific uniform resource locator and header
 import requests
 
-# url = https://alu-intranet.hbtn.io/status
+url = 'https://alu-intranet.hbtn.io/status'
 
-# # url that we are to link to
+response = requests.get(url)
 
-# response = requests.get(url)
-
-# # this is the method, get, used
-
-# print(response.status_code)
-
-# # our print statement
-
-# print(response.text)
-if __name__ == "__main__":
-    request = urllib.request.Request('https://alu-intranet.hbtn.io/status')
-    with urllib.request.urlopen(request) as response:
-        body = response.res()
-        print('Body response:')
-        print('\t- type: {}'.format(type(body)))
-        print('\t- content: {}'.format(body))
-        print('\t- utf8 content: {}'.format(body.decode('utf-8')))
+if response.status_code == 200:
+    data = response.json()
+    print("- Raw Body response:")
+    print("\t- type:", type(data))
+    print("\t- content:", data)
+else:
+    print(f"Failed to fetch data. Status code: {response.status_code}")
