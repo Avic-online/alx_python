adaabf46abfe1f68daf08d42af0727dfb22af9ee
Avic-online <avicdon1@gmail.com>
2024-02-26 15:31:33 +0100

    done
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
 """This program will import a request it is using a specific url"""
-import requests
+# import requests
 
-url = 'https://alu-intranet.hbtn.io/status'
+# url = 'https://alu-intranet.hbtn.io/status'
 
-response = requests.get(url)
+# response = requests.get(url)
 
-if response.status_code == 200:
-    data = response.json()
-    print("- Raw Body response:")
-    print("\t- type:", type(data))
-    print("\t- content:", data)
-else:
-    print(f"Failed to fetch data. Status code: {response.status_code}")
+# if response.status_code == 200:
+#     data = response.json()
+#     print("- Raw Body response:")
+#     print("\t- type:", type(data))
+#     print("\t- content:", data)
+# else:
+#     print(f"Failed to fetch data. Status code: {response.status_code}")
+
+
+import urllib.request
+
+if __name__ == "__main__":
+    request = urllib.request.Request('https://alu-intranet.hbtn.io/status')
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print('body response:')
+        print('\t- type: {}'.format(type(body)))
+        print("\t- content: {}".format(type(body)))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
