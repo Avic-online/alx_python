98b27295461c497d2078439c2a9cf6d14c511e0b
Avic-online <avicdon1@gmail.com>
2024-02-29 10:03:54 +0100

    fixed formatting
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -26,12 +26,12 @@ def get_employee_info(employee_id):
     completed_tasks = sum(task['completed'] for task in todo_data)
 
     # to output employee TODO list progress
-    print(f"Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):")
+    print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
 
     # to output titles of completed taskes
     for task in todo_data:
         if task['completed']:
-            print(f"\t{task['title']}")
+            print(f"\t {task['title']}")
 
      
     # to output TODO llist data to csv
