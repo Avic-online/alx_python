7f87a17ce7dc331e31ab8481bf54d70cef931cf3
Avic-online <avicdon1@gmail.com>
2024-03-28 21:10:57 +0100

    status code only
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,37 +1,13 @@
 #!/usr/bin/python3
-"""This program will import a request it is using a specific url"""
-# import requests
-
-# url = 'https://alu-intranet.hbtn.io/status'
-
-# response = requests.get(url)
-
-# if response.status_code == 200:
-#     data = response.json()
-#     print("- Raw Body response:")
-#     print("\t- type:", type(data))
-#     print("\t- content:", data)
-# else:
-#     print(f"Failed to fetch data. Status code: {response.status_code}")
-
-
 import requests
 
-# if __name__ == "__main__":
-#     request = requests.Request('https://intranet.hbtn.io/status')
-#     with requests.urlopen(request) as response:
-#         body = response.read()
-#         print('body response:')
-#         print('\t- type: {}'.format(type(body)))
-#         print("\t- content: {}".format(type(body)))
-#         # print("\t- utf8 content: {}".format(body.decode("utf-8")))
+def fetch_status():
+    url = 'https://alu-intranet.hbtn.io/status'
+    response = requests.get(url)
+    
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
 
-
-url = "http://intranet.hbtn.io/status"
-response = requests.get(url)
-
-# to display the body
-
-print("Body response:")
-print(f"    - type: {type(response.text).__name__}")
-print(f"    - content: {response.text}")
+if __name__ == "__main__":
+    fetch_status()
