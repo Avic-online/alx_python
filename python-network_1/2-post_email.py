23d4e3ac09b72acad0188de44d0582e04c6b8635
Avic-online <avicdon1@gmail.com>
2024-03-28 21:21:36 +0100

    post email network
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,25 +1,17 @@
-"""
-all imports are done from requests package and sys
-it is a program to send email
-"""
+#!/usr/bin/python3
+# this script posts email
 
 import requests
 import sys
 
+# function to send post requests
 def send_post_request(url, email):
-    try:
-        payload = {'email': email}
-        response = requests.post(url, data=payload)
-        print(response.text)
-    except requests.RequestException as e:
-        print("Error: {}".format(e))
-
+    # assignment and display
+    payload = {'email': email}
+    response = requests.post(url, data=payload)
+    print(response.text)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python script_name.py <URL> <email>")
-    else:
-        url = sys.argv[1]
-        email = sys.argv[2]
-
-        send_post_request(url, email)
+    url = sys.argv[1]
+    email = sys.argv[2]
+    send_post_request(url, email)
