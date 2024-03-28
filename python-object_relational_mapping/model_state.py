f28752d62cde03ac8de9ac30a98077af421c0d13
Avic-online <avicdon1@gmail.com>
2024-03-28 20:49:56 +0100

    class documentation
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -7,8 +7,8 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 # this is he class state that is inherited from Base
-
 class State(Base):
+    # this is he class state that is inherited from Base
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
